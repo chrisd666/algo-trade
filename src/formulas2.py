3343ca5e64016adc0cf4a1a455933b7a215a8cd0
chrisD666 <chris.dias@lotusis.com>
2020-10-06 23:20:30 +0530

    Classes

--- src/formulas2.py
+++ src/formulas2.py
@@ -1,13 +1,52 @@
-def MACD(data):
-    df = data.copy()
+import pandas as pd
+from nsepy import get_history
+from datetime import date
 
-    # FAST MOVING LINE
-    df['MA_Fast'] = df['Close'].ewm(span=12, min_periods=12).mean()
-    # SLOW MOVING LINE
-    df['MA_Slow'] = df['Close'].ewm(span=26, min_periods=26).mean()
-    df['MACD'] = df['MA_Fast'] - df['MA_Slow']
-    # SIGNAL
-    df['Signal'] = df['MACD'].ewm(span=9, min_periods=9).mean()
-    df.dropna(inplace=True)
+# def MACD(data, fast=12, slow=26, signal=9):
+#     df = data.copy()
 
-    return df
+#     # FAST MOVING LINE
+#     df['MA_Fast'] = df['Close'].ewm(span=fast, min_periods=fast).mean()
+#     # SLOW MOVING LINE
+#     df['MA_Slow'] = df['Close'].ewm(span=slow, min_periods=slow).mean()
+#     df['MACD'] = df['MA_Fast'] - df['MA_Slow']
+#     # SIGNAL
+#     df['Signal'] = df['MACD'].ewm(span=signal, min_periods=signal).mean()
+#     df.dropna(inplace=True)
+
+#     return df
+
+
+class Ticker:
+    def __init__(self, ticker, start_date, end_date):
+        self.ticker = ticker
+        self.start_date = start_date
+        self.end_date = end_date
+
+    def get_historical_data(self):
+        data = get_history(symbol=self.ticker,
+                           start=self.start_date, end=self.end_date)
+        data_copy = data.copy()
+        filtered_data = data_copy.drop(columns=[
+                                       'Series', 'Prev Close', 'Last', 'VWAP', 'Turnover', 'Trades', 'Deliverable Volume', '%Deliverble'])
+
+        return filtered_data
+
+
+class Indicator:
+    def __init__(self, data):
+        self.data = data
+
+    def MACD(self, fast=12, slow=26, signal=9):
+        df = self.data.copy()
+
+        # FAST MOVING LINE
+        df['MA_Fast'] = df['Close'].ewm(span=fast, min_periods=fast).mean()
+        # SLOW MOVING LINE
+        df['MA_Slow'] = df['Close'].ewm(span=slow, min_periods=slow).mean()
+        df['MACD'] = df['MA_Fast'] - df['MA_Slow']
+        # SIGNAL
+        df['Signal'] = df['MACD'].ewm(span=signal, min_periods=signal).mean()
+        df.dropna(inplace=True)
+
+        return df
