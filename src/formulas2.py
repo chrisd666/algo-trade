112922e0244c0b0c17cdcb2c61058af823d38400
chrisD666 <chris.dias@lotusis.com>
2020-10-07 12:51:34 +0530

    Slope

--- src/formulas2.py
+++ src/formulas2.py
@@ -1,20 +1,7 @@
 import pandas as pd
 from nsepy import get_history
 from datetime import date
-
-# def MACD(data, fast=12, slow=26, signal=9):
-#     df = data.copy()
-
-#     # FAST MOVING LINE
-#     df['MA_Fast'] = df['Close'].ewm(span=fast, min_periods=fast).mean()
-#     # SLOW MOVING LINE
-#     df['MA_Slow'] = df['Close'].ewm(span=slow, min_periods=slow).mean()
-#     df['MACD'] = df['MA_Fast'] - df['MA_Slow']
-#     # SIGNAL
-#     df['Signal'] = df['MACD'].ewm(span=signal, min_periods=signal).mean()
-#     df.dropna(inplace=True)
-
-#     return df
+import numpy as np
 
 
 class Ticker:
@@ -50,3 +37,25 @@ class Indicator:
         df.dropna(inplace=True)
 
         return df
+
+    def slope(self, ser, n=5):
+        import statsmodels.api as sm
+
+        slopes = [i*0 for i in range(n-1)]
+
+        for i in range(n, len(ser)+1):
+            y = ser[i-n: i]
+            x = np.array(range(n))
+
+            # SCALING
+            y_scaled = (y - y.min())/(y.max() - y.min())
+            x_scaled = (x - x.min())/(x.max() - x.min())
+            x_scaled = sm.add_constant(x_scaled)
+
+            model = sm.OLS(y_scaled, x_scaled)
+            results = model.fit()
+            slopes.append(results.params[-1])
+
+        slope_angle = np.rad2deg(np.arctan(np.array(slopes)))
+
+        return np.array(slope_angle)
